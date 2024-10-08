e55a9136cb9ad4f40a47c99d99534bac942c4d0a
Sainalkk6 <165576468+Sainalkk6@users.noreply.github.com>
2024-10-08 14:55:05 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
-i
+
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
