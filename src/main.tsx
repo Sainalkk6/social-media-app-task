e2afc5ef3565062cc4a8d4b6cc98fb4c049fc9c8
Sainalkk6 <165576468+Sainalkk6@users.noreply.github.com>
2024-10-08 14:52:59 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,13 @@ import App from './App.tsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+i
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
-        {/* <ReactQueryDevtools initialIsOpen={false}/> */}
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
